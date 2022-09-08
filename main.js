465abbea2c02424155e167f1bc3dd4eea07d6bd1
jay2viv <97814633+jay2viv@users.noreply.github.com>
2022-09-07 21:32:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload() {}
+
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+
+}
+function draw() 
+{
+    rect(40, 20, 15, 453);
+    rect(445, 20, 15, 453);
+    rect(40, 20, 431, 15);
+    rect(40, 445, 431, 15);
+    circle(50, 30, 50);
+    circle(450, 450, 50);
+    circle(50, 450, 50);
+    circle(450, 30, 50);
+}
+
+
+// the last two numbers are the size.
